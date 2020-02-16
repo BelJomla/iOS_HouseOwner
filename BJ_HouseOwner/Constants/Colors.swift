388a0c4e8796bdfc87b6091edffae2950370e4b2
Ryan-Gadhi <ryangadhi@yahoo.com>
2020-02-17 02:13:00 +0300

    Added:
    1. Constants File, that has Color Hex Values, and other General constants file ofor things like border radius values
    2. Made the main layout:
    	a. The logo
    	b. The Lables with correct colors
    	c. The textfields and nextButton with correct color

---
+++ BJ_HouseOwner/Constants/Colors.swift
@@ -0,0 +1,12 @@
+//
+//  Colors.swift
+//  BJ_HouseOwner
+//
+//  Created by Project X on 2/17/20.
+//  Copyright © 2020 beljomla.com. All rights reserved.
+//
+
+struct Colors{
+    static let gray = 0x6A6A6A
+    static let darkBlue = 0x080E33
+}
---
+++ BJ_HouseOwner/Constants/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  constants.swift
+//  BJ_HouseOwner
+//
+//  Created by Project X on 2/17/20.
+//  Copyright © 2020 beljomla.com. All rights reserved.
+//
+import CoreGraphics
+
+struct K {
+    // CGFloat only availbe in CoreGraphics
+    static let cornerRadius:CGFloat = 10.0
+}
