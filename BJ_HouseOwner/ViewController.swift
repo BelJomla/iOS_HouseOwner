ab2655fd604589ed249c5e5f197e17a83bded311
Ryan-Gadhi <ryangadhi@yahoo.com>
2020-02-16 23:39:18 +0300

    first commit

---
+++ BJ_HouseOwner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BJ_HouseOwner
+//
+//  Created by Project X on 2/16/20.
+//  Copyright © 2020 beljomla.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
