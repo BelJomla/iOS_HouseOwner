96c591729e023ff151f41e2673bbdd2a52797a82
Ryan-Gadhi <ryangadhi@yahoo.com>
2020-03-17 23:24:15 +0300

    updated the thank you for your order screen

---
+++ BJ_HouseOwner/Controllers/toBeOrganized/ThankYouOrderPlacedController.swift
@@ -0,0 +1,23 @@
+//
+//  ThankYouOrderPlacedController.swift
+//  BJ_HouseOwner
+//
+//  Created by Project X on 3/17/20.
+//  Copyright © 2020 beljomla.com. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class ThankYouOrderPlacedController: UIViewController{
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var button: UIButton!
+    override func viewDidLoad() {
+        image.layer.cornerRadius = 20
+        button.layer.cornerRadius = 20
+    }
+    
+    
+}
