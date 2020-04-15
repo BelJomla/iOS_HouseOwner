1658fec96aa9e70664c79ecc010d31ad5a5ba717
Ryan-Gadhi <ryangadhi@yahoo.com>
2020-04-16 01:38:57 +0300

    Added UIPicker instead of the not stable iosDropDown, plugged it into the LoginBrain and corrected loginbrain faults.

--- BJ_HouseOwner/Controllers/login/ViewController.swift
+++ BJ_HouseOwner/Controllers/login/ViewController.swift
@@ -12,69 +12,76 @@ import FirebaseAuth
 class ViewController: UIViewController{
     
     @IBOutlet weak var SignUpLabel: UILabel!
-
+    
     @IBOutlet weak var countryCodeTextField: UITextField!
     @IBOutlet weak var phoneTextField: UITextField!
     @IBOutlet weak var NextButton: UIButton!
     @IBOutlet weak var invalidPhoneNumber: UILabel!
     
+    var countryCodesMap:[[String]] = []// [ ['saudi +966','966'] , ['bahran +234', '234'] ...
+    
+    var defaultIndex = 0 // the index of the country codes array, 0 i.e. the first item is the default
+    
+    
     override func viewDidLoad() {
-           super.viewDidLoad()
-           styleUI() //
-            // ERROR
-//            DB.getUser(withID: "2P6ANOpEfUUkF3Q0gf5aJExnzJH3")
-//  --------------------------------
-//        PhoneAuthProvider.provider().verifyPhoneNumber("+966534225804", uiDelegate: nil) { (verificationID, error) in
-//          if let error = error {
-//            Logger.log(.error, " phone auth \(error)")
-//            return
-//          }
-//          // Sign in using the verificationID and the code sent to the user
-//          // ...
-//            print("verification id: \(verificationID)")
-//        }
-//  --------------------------------
-//       DB.getCategories(){
-//            categories in
-//            print("in^^^")
-//            for cat in categories{
-//                cat.toString()
-//            }
-//        }
-//  --------------------------------
-//        DB.getProducts(withSubCollectionID: "4_2"){
-//            products in
-//
-//            Logger.log(.success, "products has been got")
-//            for product in products {
-//                product.toString()
-//            }
-//
-//        }
-//  --------------------------------
-        SettingsUserDefaults.setPreferedLanguage(language: .Arabic)
+        super.viewDidLoad()
         
-        let lang = SettingsUserDefaults.defualts.string(forKey: "language")
+        let countryPicker = UIPickerView()
+        countryPicker.dataSource = self
+        countryPicker.delegate = self
+        countryCodeTextField.inputView = countryPicker
+        styleUI()
+        self.countryCodesMap = self.getCountryCodes()
         
         
-        Logger.log(.success, " success in persisting data. lang: \(lang)")
-    
+        
+        //
+        // ERROR
+        //            DB.getUser(withID: "2P6ANOpEfUUkF3Q0gf5aJExnzJH3")
+        //  --------------------------------
+        //        PhoneAuthProvider.provider().verifyPhoneNumber("+966534225804", uiDelegate: nil) { (verificationID, error) in
+        //          if let error = error {
+        //            Logger.log(.error, " phone auth \(error)")
+        //            return
+        //          }
+        //          // Sign in using the verificationID and the code sent to the user
+        //          // ...
+        //            print("verification id: \(verificationID)")
+        //        }
+        //  --------------------------------
+        //       DB.getCategories(){
+        //            categories in
+        //            print("in^^^")
+        //            for cat in categories{
+        //                cat.toString()
+        //            }
+        //        }
+        //  --------------------------------
+        //        DB.getProducts(withSubCollectionID: "4_2"){
+        //            products in
+        //
+        //            Logger.log(.success, "products has been got")
+        //            for product in products {
+        //                product.toString()
+        //            }
+        //
+        //        }
+        //  --------------------------------
+        //        SettingsUserDefaults.setPreferedLanguage(language: .Arabic)
+        //
+        //        let lang = SettingsUserDefaults.defualts.string(forKey: "language")
+        //
+        //
+        //        Logger.log(.success, " success in persisting data. lang: \(lang)")
+        //
     }
     
-    @IBAction func nextButtonPressed(_ sender: UIButton) {
-        //NOTE: -Can use prepare for segue if needed
-        if let phoneNumber = phoneTextField.text {
-            LogInBrain.phoneNumber = phoneNumber
-            if LogInBrain.checkPhoneNumber() {
-                performSegue(withIdentifier: K.verifyPhoneSegue, sender: self)
-            } else {
-                invalidPhoneNumber.textColor = UIColor(rgb: Colors.red)
-            }
-        }
-    }
+    //  --------------------------------
     
-
-
+    
+    
+    
+    
     /**
      This fuction updates the UI look and feel. Reason: hard to do using options
      // reads json file, and returns country codes dictionary, ex: Saudi Arabia -> +966
@@ -94,57 +101,84 @@ class ViewController: UIViewController{
         NextButton.layer.cornerRadius = K.cornerRadius
     }
     
+    @IBAction func nextButtonPressed(_ sender: UIButton) {
+        //NOTE: -Can use prepare for segue if needed
+        if let phoneNumber = phoneTextField.text  {
+            LogInBrain.phoneNumber = phoneNumber
+            LogInBrain.countryCode = countryCodesMap[defaultIndex][1]
+            if LogInBrain.checkPhoneNumber() {
+                invalidPhoneNumber.isHidden = true
+                performSegue(withIdentifier: K.verifyPhoneSegue, sender: self)
+            } else {
+                invalidPhoneNumber.isHidden = false
+                invalidPhoneNumber.textColor = UIColor(rgb: Colors.red)
+            }
+        }
+    }
     
-//MARK: - DropDown & country Codes
+    //MARK: - DropDown & country Codes
     
-//    func addCountryDropDownMenue (){
-//
-//        let countryCodes = getCountryCodes()! // this is a dictionary
-//
-//        // here we take the (Dictionary<Key, Value>.Values) type
-//        // and convert it to array so we can manipulate it
-//        // the array will contain the contry codes as [String]
-//        let  countryCodesArr: [String] = Array(countryCodes.values)
-//
-//        // casting country code string to int, ex: "966" -> 966
-//        // here we cast any string that can't be convert to Int to -1
-//        // from our json that never happens, but swift requires this type of handling
-//       let countryCodesArrInt  = countryCodesArr.map { (stringCode) -> Int in
-//            (Int(stringCode) ?? -1)
-//        }
-//
-//
+    func getCountryCodes () -> ([[String]]){
+        
+        let countryCodes = readCountryCodes()! // this is a dictionary
+        
+        // here we take the (Dictionary<Key, Value>.Values) type
+        // and convert it to array so we can manipulate it
+        // the array will contain the contry codes as [String]
+        let  countryCodesArr: [String] = Array(countryCodes.values)
+        
+        // casting country code string to int, ex: "966" -> 966
+        // here we cast any string that can't be convert to Int to -1
+        // from our json that never happens, but swift requires this type of handling
+        let countryCodesArrInt  = countryCodesArr.map { (stringCode) -> Int in
+            (Int(stringCode) ?? -1)
+        }
+        
+        
 //        // This is the acutal list of options that is displayed to the user
-//        countryCodeTextField.optionArray = countryCodesArrInt.map({ (code) -> String in
+//        let codesWithCountryName = countryCodesArrInt.map({ (code) -> String in
 //            let countryName = (countryCodes.someKey(forValue: String(code)) ?? "--")
-//            return "\(countryName)   (+\(code))"
-//        })
+//            return "\(countryName) (+\(code))"
+//            }
+//        )
 //
-//        countryCodeTextField.optionIds = countryCodesArrInt
-//
-//        // the color that shows up after the user chooses an option
-//        countryCodeTextField.selectedRowColor = .white
-//        //countryCodeTextField.showList()  // To show the Drop Down Menu
-//
-//        // just printing what the user clicked (not important)
-//        countryCodeTextField.didSelect{(selectedText , index ,id) in
-//        let selected = "Selected String: \(selectedText) \n index: \(index) \n \(id)"
-//            print(selected)
-//            LogInBrain.countryCode = String("+\(id)")
-//        }
-//    }
+        
+        //map
+        // This is the acutal list of options that is displayed to the user
+        let codesMap = countryCodesArrInt.map({ (code) -> [String] in
+            let countryName = (countryCodes.someKey(forValue: String(code)) ?? "--")
+            return ["\(countryName) (+\(code))", "\(code)"]
+            }
+        )
+        
+        
+        
+//        _ = countryCodesArrInt.map({ (code) -> String in
+//                   return "\(code)"
+//                   }
+//               )
+        
+        return codesMap
+    }
+    
+    
+    
+    
+    
     
     /**
-        This method reads country codes in countryCodes.json
-            Returns: a dictionary, mapping country -> code
-                ex: "saudi arabia" -> "966"
+     This method reads country codes in countryCodes.json
+     Returns: a dictionary, mapping country -> code
+     ex: "saudi arabia" -> "966"
      
      */
-    func getCountryCodes() -> [String:String]? {
+    func readCountryCodes() -> [String:String]? {
         // this instance is going to parse json
         let decoder = JSONDecoder()
         // this path is String? and is the easiest wasy to locate a file
         let path = Bundle.main.path(forResource: "countryCodes", ofType: "json")
+        
+        
         // this url object is required to genrate Data object
         let url = URL(fileURLWithPath: path!)
         
@@ -154,10 +188,10 @@ class ViewController: UIViewController{
             // this is the result of the decoded json
             let decoded = try decoder.decode(codes.self, from: data )
             // this is the way it should be accessed -> decoded.countryCodes[0].name
-           
+            
             
             //FIXME: -This is hardcoded, find a better way
-            let coutryCount = 248
+            let coutryCount = 7
             var countryCodes :[String:String] = [:]
             
             for index in (0..<coutryCount) {
@@ -176,40 +210,62 @@ class ViewController: UIViewController{
             return nil
         }
         
-    
+        
     }
     
     
-
-
+    
+    
 }
+
+//MARK: -UIPicker
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        countryCodesMap.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        countryCodesMap[row][0]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        defaultIndex = row
+        countryCodeTextField.text = countryCodesMap[row][0]
+    }
+    
+}
+
 
 //MARK: - Swift Extenstions
 
 /**
-        returns a UIColor after getting an rgb value
+ returns a UIColor after getting an rgb value
  */
 extension UIColor {
-   convenience init(red: Int, green: Int, blue: Int) {
-       assert(red >= 0 && red <= 255, "Invalid red component")
-       assert(green >= 0 && green <= 255, "Invalid green component")
-       assert(blue >= 0 && blue <= 255, "Invalid blue component")
-
-       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
-   }
-
-   convenience init(rgb: Int) {
-       self.init(
-           red: (rgb >> 16) & 0xFF,
-           green: (rgb >> 8) & 0xFF,
-           blue: rgb & 0xFF
-       )
-   }
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
 }
 
 /** This extenstion adds the ability of getting a key from value in a ditionary
-        ex: suppose we have this dict ['a' -> 1] and it is called myDict
-             to get 'a', we do myDict.someKey(1)
+ ex: suppose we have this dict ['a' -> 1] and it is called myDict
+ to get 'a', we do myDict.someKey(1)
  */
 extension Dictionary where Value: Equatable {
     func someKey(forValue val: Value) -> Key? {
